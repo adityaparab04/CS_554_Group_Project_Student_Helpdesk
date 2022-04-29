b3f0e6829c2194d29791ad1af8c99f0845ba2d80
KellyD212 <direstok@gmail.com>
2022-04-29 14:42:59 -0400

    firebase functions

--- ticket_frontend/src/firebase/FirebaseFunctions.js
+++ ticket_frontend/src/firebase/FirebaseFunctions.js
@@ -1,10 +1,10 @@
 import firebaseApp from "./Firebase";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,updateProfile, signOut  } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, signOut } from "firebase/auth";
 const auth = getAuth(firebaseApp);
 
-async function doCreateUserWithEmailAndPassword(email, password, diplayName){
-    await createUserWithEmailAndPassword(auth,email, password);
-    updateProfile(auth.currentUser, {displayName: diplayName});
+async function doCreateUserWithEmailAndPassword(email, password, diplayName) {
+    await createUserWithEmailAndPassword(auth, email, password);
+    updateProfile(auth.currentUser, { displayName: diplayName });
     console.log("User Created having UID:", auth.currentUser.uid)
 }
 
@@ -13,13 +13,45 @@ async function doSignInWithEmailAndPassword(email, password) {
     console.log("User Signed-In having UID:", auth.currentUser.uid)
 }
 
-async function doSignOut(){
+async function doSignOut() {
     await signOut(auth);
     console.log("User Signed-Out")
+}
+
+async function doChangePassword(email, oldPassword, newPassword) {
+    let credential = (auth.EmailAuthProvider.credential,
+        email,
+        oldPassword
+    );
+    await reauthenticateWithCredential(auth.currentUser, credential);
+    await updatePassword(auth.currentUser, newPassword);
+    await doSignOut();
+}
+
+async function doSocialSignIn(provider) {
+    let socialProvider = null;
+    if (provider === 'google') {
+        socialProvider = new GoogleAuthProvider(auth);
+    } else if (provider === 'facebook') {
+        socialProvider = new FacebookAuthProvider(auth);
+    }
+    await signInWithPopup(auth, socialProvider);
+}
+
+async function doPasswordReset(email) {
+    await sendPasswordResetEmail(auth, email);
+}
+
+async function doPasswordUpdate(password) {
+    await updatePassword(auth, password);
 }
 
 export {
     doCreateUserWithEmailAndPassword,
     doSignInWithEmailAndPassword,
     doSignOut,
+    doChangePassword,
+    doSocialSignIn,
+    doPasswordUpdate,
+    doPasswordReset
 }
